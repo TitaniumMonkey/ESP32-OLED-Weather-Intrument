e4e614bab0007d46288d6b930335b1c20d8ff62f
TitaniumMonkey <31460002+TitaniumMonkey@users.noreply.github.com>
2025-02-02 15:05:00 -0800

    Create secrets.h
    -added secrets.h template file

---
+++ include/secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+#define MQTT_SERVER "your_mqtt_broker_ip"
+#define MQTT_USER "your_mqtt_username"
+#define MQTT_PASS "your_mqtt_password"
+
+#endif
