7b25c1fd80eeec7e22170ea0f7089082318c620c
TitaniumMonkey <31460002+TitaniumMonkey@users.noreply.github.com>
2025-02-02 15:48:31 -0800

    Update secrets.h
    -added more comments to sercrets.h

--- include/secrets.h
+++ include/secrets.h
@@ -1,10 +1,13 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-#define WIFI_SSID "your_wifi_ssid"
-#define WIFI_PASSWORD "your_wifi_password"
-#define MQTT_SERVER "your_mqtt_broker_ip"
-#define MQTT_USER "your_mqtt_username"
-#define MQTT_PASS "your_mqtt_password"
+// Wi-Fi credentials
+#define WIFI_SSID "your_wifi_ssid"         // Wi-Fi network name
+#define WIFI_PASSWORD "your_wifi_password" // Wi-Fi network password
+
+// MQTT broker credentials
+#define MQTT_SERVER "your_mqtt_broker_ip"  // MQTT broker IP address or hostname
+#define MQTT_USER "your_mqtt_username"     // MQTT username
+#define MQTT_PASS "your_mqtt_password"     // MQTT password
 
 #endif
